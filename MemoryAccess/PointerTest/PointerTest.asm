300236fc1c0727784b495a061e8c7b2dcccc96ba
a.abduraupov <abduraupovalisher790@gmail.com>
2024-10-31 13:04:50 +0500

    as

---
+++ MemoryAccess/PointerTest/PointerTest.asm
@@ -0,0 +1,15 @@
+// C_PUSH constant 3030
+// C_POP pointer 0
+// C_PUSH constant 3040
+// C_POP pointer 1
+// C_PUSH constant 32
+// C_POP this 2
+// C_PUSH constant 46
+// C_POP that 6
+// C_PUSH pointer 0
+// C_PUSH pointer 1
+// add
+// C_PUSH this 2
+// sub
+// C_PUSH that 6
+// add
